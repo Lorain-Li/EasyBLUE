1c9938a80c3c4d4216ec7547b74d83607e4176fc
lorainlynies <lorainlynies@outlook.com>
2016-11-09 23:16:22 +0800

    base files.

---
+++ EasyBLUE/EasyBLUE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyBLUE
+//
+//  Created by Lorain on 2016/11/9.
+//  Copyright © 2016年 LorainLynies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyBLUE/EasyBLUE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EasyBLUE
+//
+//  Created by Lorain on 2016/11/9.
+//  Copyright © 2016年 LorainLynies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
